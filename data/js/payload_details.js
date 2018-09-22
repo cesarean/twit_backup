33459bc0ede25c82af6a0c413f59cc2d0434ef80
cesarean <cesareacs@gmail.com>
2018-09-23 02:25:16 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23332,
-	"created_at": "Fri Sep 21 17:18:12 +0000 2018",
+	"tweets": 23335,
+	"created_at": "Sat Sep 22 17:25:15 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_09.js
+++ data/js/tweets/2018_09.js
@@ -1,6 +1,700 @@
 Grailbird.data.tweets_2018_09 = 
 [
 	{
+		"created_at": "Sat Sep 22 17:17:52 +0000 2018",
+		"id": 1043549955971899400,
+		"id_str": "1043549955971899392",
+		"text": "RT @dongwoona92: #러브앤프로듀서 #恋与制作人\n럽프듀 카피페(@evol_ccp)🙄💛\n헤헿 웃겨서 나도 그렷다 https://t.co/TSQDvFYfRK",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "러브앤프로듀서",
+					"indices": [
+						17,
+						25
+					]
+				},
+				{
+					"text": "恋与制作人",
+					"indices": [
+						26,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dongwoona92",
+					"name": "동우나",
+					"id": 4718851122,
+					"id_str": "4718851122",
+					"indices": [
+						3,
+						15
+					]
+				},
+				{
+					"screen_name": "evol_ccp",
+					"name": "럽프듀 카피페봇",
+					"id": 1018435177297559600,
+					"id_str": "1018435177297559556",
+					"indices": [
+						41,
+						50
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1039688294554984400,
+					"id_str": "1039688294554984448",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+					"url": "https://t.co/TSQDvFYfRK",
+					"display_url": "pic.twitter.com/TSQDvFYfRK",
+					"expanded_url": "https://twitter.com/dongwoona92/status/1039688308895281153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1515,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1039688308895281200,
+					"source_status_id_str": "1039688308895281153",
+					"source_user_id": 4718851122,
+					"source_user_id_str": "4718851122"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1039688294554984400,
+					"id_str": "1039688294554984448",
+					"indices": [
+						68,
+						91
+					],
+					"media_url": "http://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+					"url": "https://t.co/TSQDvFYfRK",
+					"display_url": "pic.twitter.com/TSQDvFYfRK",
+					"expanded_url": "https://twitter.com/dongwoona92/status/1039688308895281153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1515,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1039688308895281200,
+					"source_status_id_str": "1039688308895281153",
+					"source_user_id": 4718851122,
+					"source_user_id_str": "4718851122"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★/핀웨페아/핀마에/길켈/잉귀페아 etc 핀골피니안x페아노리안 위주",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 29,
+			"friends_count": 50,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5197,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23136,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Sep 12 01:33:03 +0000 2018",
+			"id": 1039688308895281200,
+			"id_str": "1039688308895281153",
+			"text": "#러브앤프로듀서 #恋与制作人\n럽프듀 카피페(@evol_ccp)🙄💛\n헤헿 웃겨서 나도 그렷다 https://t.co/TSQDvFYfRK",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "러브앤프로듀서",
+						"indices": [
+							0,
+							8
+						]
+					},
+					{
+						"text": "恋与制作人",
+						"indices": [
+							9,
+							15
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "evol_ccp",
+						"name": "럽프듀 카피페봇",
+						"id": 1018435177297559600,
+						"id_str": "1018435177297559556",
+						"indices": [
+							24,
+							33
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1039688294554984400,
+						"id_str": "1039688294554984448",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+						"url": "https://t.co/TSQDvFYfRK",
+						"display_url": "pic.twitter.com/TSQDvFYfRK",
+						"expanded_url": "https://twitter.com/dongwoona92/status/1039688308895281153/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 713,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 404,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 900,
+								"h": 1515,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1039688294554984400,
+						"id_str": "1039688294554984448",
+						"indices": [
+							51,
+							74
+						],
+						"media_url": "http://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Dm22_tIU4AAx5y2.jpg",
+						"url": "https://t.co/TSQDvFYfRK",
+						"display_url": "pic.twitter.com/TSQDvFYfRK",
+						"expanded_url": "https://twitter.com/dongwoona92/status/1039688308895281153/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 713,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 404,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 900,
+								"h": 1515,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4718851122,
+				"id_str": "4718851122",
+				"name": "동우나",
+				"screen_name": "dongwoona92",
+				"location": "",
+				"description": "FUB FREE | 피키툰 쓰리스쿨즈 연재 중 | oioi5500@naver.com",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 702,
+				"friends_count": 13,
+				"listed_count": 1,
+				"created_at": "Wed Jan 06 14:40:03 +0000 2016",
+				"favourites_count": 97,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 483,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "4A913C",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1041868980917493760/Tx7SVt8R_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1041868980917493760/Tx7SVt8R_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4718851122/1537235939",
+				"profile_link_color": "10E4D8",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 523,
+			"favorite_count": 521,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 523,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
+		"created_at": "Sat Sep 22 17:12:38 +0000 2018",
+		"id": 1043548641154093000,
+		"id_str": "1043548641154093056",
+		"text": "빠른 이동하면 이렇게 지하철 타는 클립이 나오는데 이건 스파이디 코스한 사람 만난 거ㅋㅋㅋ 후드 입은 게 나다&lt; https://t.co/TiP36QOrJv",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1043548638343909400,
+					"id_str": "1043548638343909376",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/Dntt9SZU0AAK92I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dntt9SZU0AAK92I.jpg",
+					"url": "https://t.co/TiP36QOrJv",
+					"display_url": "pic.twitter.com/TiP36QOrJv",
+					"expanded_url": "https://twitter.com/celebaduial/status/1043548641154093056/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1043548638343909400,
+					"id_str": "1043548638343909376",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/media/Dntt9SZU0AAK92I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dntt9SZU0AAK92I.jpg",
+					"url": "https://t.co/TiP36QOrJv",
+					"display_url": "pic.twitter.com/TiP36QOrJv",
+					"expanded_url": "https://twitter.com/celebaduial/status/1043548641154093056/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.playstation.com\" rel=\"nofollow\">PlayStation(R)4</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★/핀웨페아/핀마에/길켈/잉귀페아 etc 핀골피니안x페아노리안 위주",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 29,
+			"friends_count": 50,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5197,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23136,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
+		"created_at": "Sat Sep 22 17:09:51 +0000 2018",
+		"id": 1043547937542824000,
+		"id_str": "1043547937542823937",
+		"text": "새로 산 마블 스파이더맨 게임. 어벤저스 타워가 보인다. 호평중이길래 샀더니 재밌다. 날아다니는 것도 시원하고. https://t.co/2Ps7xzdGy8",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1043547934531366900,
+					"id_str": "1043547934531366913",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DnttUUfVsAE3krz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnttUUfVsAE3krz.jpg",
+					"url": "https://t.co/2Ps7xzdGy8",
+					"display_url": "pic.twitter.com/2Ps7xzdGy8",
+					"expanded_url": "https://twitter.com/celebaduial/status/1043547937542823937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1043547934531366900,
+					"id_str": "1043547934531366913",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DnttUUfVsAE3krz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DnttUUfVsAE3krz.jpg",
+					"url": "https://t.co/2Ps7xzdGy8",
+					"display_url": "pic.twitter.com/2Ps7xzdGy8",
+					"expanded_url": "https://twitter.com/celebaduial/status/1043547937542823937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://www.playstation.com\" rel=\"nofollow\">PlayStation(R)4</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★/핀웨페아/핀마에/길켈/잉귀페아 etc 핀골피니안x페아노리안 위주",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 29,
+			"friends_count": 50,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5197,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23136,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/948255031291133952/QC1_dgeW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Fri Sep 21 08:04:58 +0000 2018",
 		"id": 1043048427347529700,
 		"id_str": "1043048427347529728",
