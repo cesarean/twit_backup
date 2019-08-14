3d0e520bf3cfc8fddfb97db3f38b867192122809
cesarean <cesareacs@gmail.com>
2019-08-15 02:18:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23953,
-	"created_at": "Tue Aug 06 17:18:12 +0000 2019",
+	"tweets": 23954,
+	"created_at": "Wed Aug 14 17:18:11 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 14 10:11:56 +0000 2019",
+		"id": 1161581212554547200,
+		"id_str": "1161581212554547201",
+		"full_text": "@interlaken_ 또 누가 있었죠..장정일도 있고 정비석도 있고..근데 저는 이문열을 제외한 다른 삼국지 일단 아무거나 봐도 된다고 생각하는 편입니다. 어차피 삼국지는 빠지게 되면 모든 자료를 다 읽게 되므로..",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			121
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "interlaken_",
+					"name": "🦄",
+					"id": 1076013290491277300,
+					"id_str": "1076013290491277312",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twicca.r246.jp/\" rel=\"nofollow\">twicca</a>",
+		"in_reply_to_status_id": 1161580356237062100,
+		"in_reply_to_status_id_str": "1161580356237062144",
+		"in_reply_to_user_id": 1076013290491277300,
+		"in_reply_to_user_id_str": "1076013290491277312",
+		"in_reply_to_screen_name": "interlaken_",
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★/핀웨페아/핀마에/길켈/잉귀페아 etc 핀골피니안x페아노리안 위주",
+			"url": "https://t.co/stRBtbNmuQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/stRBtbNmuQ",
+							"expanded_url": "http://glanloth.tistory.com",
+							"display_url": "glanloth.tistory.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 28,
+			"friends_count": 51,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5222,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23627,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102779554416930816/RYnHnszD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102779554416930816/RYnHnszD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "@interlaken_ 또 누가 있었죠..장정일도 있고 정비석도 있고..근데 저는 이문열을 제외한 다른 삼국지 일단 아무거나 봐도 된다고 생각하는 편입니다. 어차피 삼국지는 빠지게 되면 모든 자료를 다 읽게 되므로.."
+	},
+	{
 		"created_at": "Tue Aug 06 11:50:05 +0000 2019",
 		"id": 1158706806404706300,
 		"id_str": "1158706806404706304",
