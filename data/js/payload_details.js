b21b2baa8d29557bdc5990c73a7b130de818f7ff
cesarean <cesareacs@gmail.com>
2020-04-28 02:18:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24106,
-	"created_at": "Sat Apr 25 17:18:11 +0000 2020",
+	"tweets": 24107,
+	"created_at": "Mon Apr 27 17:18:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,193 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 27 11:53:35 +0000 2020",
+		"id": 1254740473387729000,
+		"id_str": "1254740473387728902",
+		"full_text": "입덕은 위무선으로 했는데 과몰입의 문은 섭회상으로 닫게 된...회상아 사랑해!! https://t.co/cvq9SO3iA1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1254740443184554000,
+					"id_str": "1254740443184553984",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EWm7yQFU4AAn-bI.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EWm7yQFU4AAn-bI.jpg",
+					"url": "https://t.co/cvq9SO3iA1",
+					"display_url": "pic.twitter.com/cvq9SO3iA1",
+					"expanded_url": "https://twitter.com/celebaduial/status/1254740473387728902/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1254740443184554000,
+					"id_str": "1254740443184553984",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EWm7yQFU4AAn-bI.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EWm7yQFU4AAn-bI.jpg",
+					"url": "https://t.co/cvq9SO3iA1",
+					"display_url": "pic.twitter.com/cvq9SO3iA1",
+					"expanded_url": "https://twitter.com/celebaduial/status/1254740473387728902/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"large": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 580,
+							"h": 580,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EWm7yQFU4AAn-bI.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1254724143762690000,
+		"in_reply_to_status_id_str": "1254724143762690048",
+		"in_reply_to_user_id": 2327283314,
+		"in_reply_to_user_id_str": "2327283314",
+		"in_reply_to_screen_name": "celebaduial",
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★/핀웨페아/핀마에/길켈/잉귀페아 etc 핀골피니안x페아노리안 위주",
+			"url": "https://t.co/stRBtbNmuQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/stRBtbNmuQ",
+							"expanded_url": "http://glanloth.tistory.com",
+							"display_url": "glanloth.tistory.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 29,
+			"friends_count": 70,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5389,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23803,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102779554416930816/RYnHnszD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102779554416930816/RYnHnszD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "입덕은 위무선으로 했는데 과몰입의 문은 섭회상으로 닫게 된...회상아 사랑해!! https://t.co/cvq9SO3iA1"
+	},
+	{
 		"created_at": "Sat Apr 25 09:42:59 +0000 2020",
 		"id": 1251531433249656800,
 		"id_str": "1253982831346282497",
