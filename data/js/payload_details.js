cc918d604d8236790faef191ee871578c2c8d3c1
cesarean <cesareacs@gmail.com>
2020-06-12 02:18:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24138,
-	"created_at": "Sun Jun 07 17:18:10 +0000 2020",
+	"tweets": 24139,
+	"created_at": "Thu Jun 11 17:18:11 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Thu Jun 11 03:41:34 +0000 2020",
+		"id": 1244647962803748900,
+		"id_str": "1270924108700676096",
+		"full_text": "위진시대 남자들 화장법, 이 영상을 보고 왜 위진시대에 남색이 흥했는지 깨달았다 내가 남자였어도 양쪽에 남자끼고 놀았다 https://t.co/QtmPA1PvyE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1244647859779035100,
+					"id_str": "1244647859779035136",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1244647859779035136/pu/img/rMF97424QjSqV2sr.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1244647859779035136/pu/img/rMF97424QjSqV2sr.jpg",
+					"url": "https://t.co/QtmPA1PvyE",
+					"display_url": "pic.twitter.com/QtmPA1PvyE",
+					"expanded_url": "https://twitter.com/suhoxin/status/1244647962803748865/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1244647859779035100,
+					"id_str": "1244647859779035136",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1244647859779035136/pu/img/rMF97424QjSqV2sr.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1244647859779035136/pu/img/rMF97424QjSqV2sr.jpg",
+					"url": "https://t.co/QtmPA1PvyE",
+					"display_url": "pic.twitter.com/QtmPA1PvyE",
+					"expanded_url": "https://twitter.com/suhoxin/status/1244647962803748865/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 44400,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1244647859779035136/pu/vid/320x568/GMJifxcPZxIecHu3.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1244647859779035136/pu/pl/tVpEQGuIxDNogPoG.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1244647859779035136/pu/vid/720x1280/3awMGiNV3bhWAgsX.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1244647859779035136/pu/vid/360x640/uYKxGYEOUZEXurJH.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4378466360,
+			"id_str": "4378466360",
+			"name": "会长",
+			"screen_name": "suhoxin",
+			"location": "姽婳城 听竹园",
+			"description": "陌上人如玉，公子世无双。  |  头像画师是千景绘  |  성인  |  中国人🇨🇳",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1967,
+			"friends_count": 105,
+			"listed_count": 6,
+			"created_at": "Fri Nov 27 14:49:24 +0000 2015",
+			"favourites_count": 8805,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5973,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1181461897863786496/PvlSo1Gg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181461897863786496/PvlSo1Gg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4378466360/1570517341",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15779,
+		"favorite_count": 8483,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "위진시대 남자들 화장법, 이 영상을 보고 왜 위진시대에 남색이 흥했는지 깨달았다 내가 남자였어도 양쪽에 남자끼고 놀았다 https://t.co/QtmPA1PvyE"
+	},
+	{
 		"created_at": "Sun Jun 07 14:52:14 +0000 2020",
 		"id": 1269643332868694000,
 		"id_str": "1269643332868694018",
