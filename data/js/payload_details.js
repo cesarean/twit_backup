c16e284b62c6f22c47fd2e9d283000b9268f2d33
cesarean <cesareacs@gmail.com>
2020-08-10 02:18:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24144,
-	"created_at": "Wed Jul 22 17:18:11 +0000 2020",
+	"tweets": 24145,
+	"created_at": "Sun Aug 09 17:18:11 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,164 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Sun Aug 09 13:03:08 +0000 2020",
+		"id": 1292446316128174000,
+		"id_str": "1292446316128174084",
+		"full_text": "내일 출근길 또 엄청 막히겠네 시바... https://t.co/f6kmN1IvQQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1292446313498284000,
+					"id_str": "1292446313498284035",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee-xEw9UEAMxJID.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee-xEw9UEAMxJID.jpg",
+					"url": "https://t.co/f6kmN1IvQQ",
+					"display_url": "pic.twitter.com/f6kmN1IvQQ",
+					"expanded_url": "https://twitter.com/celebaduial/status/1292446316128174084/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1292446313498284000,
+					"id_str": "1292446313498284035",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee-xEw9UEAMxJID.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee-xEw9UEAMxJID.jpg",
+					"url": "https://t.co/f6kmN1IvQQ",
+					"display_url": "pic.twitter.com/f6kmN1IvQQ",
+					"expanded_url": "https://twitter.com/celebaduial/status/1292446316128174084/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 337,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★핀골피니안x페아노리안 위주/(진정령) 명결회상 중심/성인⬆️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 29,
+			"friends_count": 65,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5417,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23848,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "내일 출근길 또 엄청 막히겠네 시바... https://t.co/f6kmN1IvQQ"
+	}
+]
