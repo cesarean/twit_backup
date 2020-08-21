8138f23512ff9a5e9f53855bef8b7d95633bcab3
cesarean <cesareacs@gmail.com>
2020-08-22 02:18:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24145,
-	"created_at": "Sun Aug 09 17:18:11 +0000 2020",
+	"tweets": 24146,
+	"created_at": "Fri Aug 21 17:18:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 21 04:20:28 +0000 2020",
+		"id": 1296385900176269300,
+		"id_str": "1296663434872807426",
+		"full_text": "탐라 보고 생각난 저의 비장의 힐링영상 ㅠㅠ \n\n지금까지도 힐링되고 있는 영상 ㅠㅠㅠㅠㅠㅠ https://t.co/ON2Tr1BFL7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1296385826268442600,
+					"id_str": "1296385826268442625",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1296385826268442625/pu/img/QRNa0fxF_CG7uj_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1296385826268442625/pu/img/QRNa0fxF_CG7uj_N.jpg",
+					"url": "https://t.co/ON2Tr1BFL7",
+					"display_url": "pic.twitter.com/ON2Tr1BFL7",
+					"expanded_url": "https://twitter.com/goc_94_/status/1296385900176269313/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1296385826268442600,
+					"id_str": "1296385826268442625",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1296385826268442625/pu/img/QRNa0fxF_CG7uj_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1296385826268442625/pu/img/QRNa0fxF_CG7uj_N.jpg",
+					"url": "https://t.co/ON2Tr1BFL7",
+					"display_url": "pic.twitter.com/ON2Tr1BFL7",
+					"expanded_url": "https://twitter.com/goc_94_/status/1296385900176269313/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 63900,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1296385826268442625/pu/vid/360x640/AN3T4XT8SI46UpO7.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1296385826268442625/pu/vid/320x568/_M1Vh-oGcPyPxe_3.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1296385826268442625/pu/vid/720x1280/9n3R5N5H9GFU1j3I.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1296385826268442625/pu/pl/_FKgs9n86QVG4Fqr.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1286610753961644000,
+			"id_str": "1286610753961644032",
+			"name": "ෆɴᴅෆ",
+			"screen_name": "goc_94_",
+			"location": "锅巴",
+			"description": "중알못 덕후의 자급자족 #郭丞 & 중연 덕질계｜자막영상 재업로드❌｜🔕",
+			"url": "https://t.co/1mwdQXr7IR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1mwdQXr7IR",
+							"expanded_url": "http://youtube.com/c/ONDOchannel_for940818",
+							"display_url": "youtube.com/c/ONDOchannel_…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294,
+			"friends_count": 37,
+			"listed_count": 2,
+			"created_at": "Fri Jul 24 10:34:51 +0000 2020",
+			"favourites_count": 938,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1383,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1295742110117683200/JrBnLdbI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1295742110117683200/JrBnLdbI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1286610753961644032/1595587064",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 489,
+		"favorite_count": 581,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "탐라 보고 생각난 저의 비장의 힐링영상 ㅠㅠ \n\n지금까지도 힐링되고 있는 영상 ㅠㅠㅠㅠㅠㅠ https://t.co/ON2Tr1BFL7"
+	},
+	{
 		"created_at": "Sun Aug 09 13:03:08 +0000 2020",
 		"id": 1292446316128174000,
 		"id_str": "1292446316128174084",
