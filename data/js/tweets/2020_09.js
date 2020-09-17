fe845a505b269d2a029435f8514f64604f790e25
cesarean <cesareacs@gmail.com>
2020-09-18 02:18:12 +0900

    Added 1 tweets

--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 17 04:48:13 +0000 2020",
+		"id": 1306430511003263000,
+		"id_str": "1306454890936332290",
+		"full_text": "공무원은 내 세금을 얼마나 받아먹을까? https://t.co/EbQePTqP7d - @YouTube 충주시의 미친 영상 ㅋㅋㅋㅋ ‘너 내가 낸 세금으로 월급받는거여!’ 하는 분들은 연간 약 0.48원 정도를 그 공무원에게 주고 있다고 하네요…",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						48,
+						56
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/EbQePTqP7d",
+					"expanded_url": "https://youtu.be/EgIus_6EMeU",
+					"display_url": "youtu.be/EgIus_6EMeU",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 571420512,
+			"id_str": "571420512",
+			"name": "ㅅㅗㄹㅏㄱㅔ",
+			"screen_name": "raghe_o",
+			"location": "11.12.16",
+			"description": "CS Student. Barista. Gardener @raghe_g. WoW Player. Soraghe-Azshara(KR). Hall of Fame Ny’alotha #93.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 664,
+			"friends_count": 1018,
+			"listed_count": 6,
+			"created_at": "Sat May 05 01:55:39 +0000 2012",
+			"favourites_count": 7345,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 78722,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1253596246196936704/a-MpfGy-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1253596246196936704/a-MpfGy-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/571420512/1570808721",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2543,
+		"favorite_count": 460,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "공무원은 내 세금을 얼마나 받아먹을까? https://t.co/EbQePTqP7d - @YouTube 충주시의 미친 영상 ㅋㅋㅋㅋ ‘너 내가 낸 세금으로 월급받는거여!’ 하는 분들은 연간 약 0.48원 정도를 그 공무원에게 주고 있다고 하네요…"
+	},
+	{
 		"created_at": "Wed Sep 16 13:31:37 +0000 2020",
 		"id": 1306224222029504500,
 		"id_str": "1306224222029504512",
