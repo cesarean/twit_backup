9c17af7f6c059c891c29c043747b04d390869453
cesarean <cesareacs@gmail.com>
2020-12-22 02:18:11 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24173,
-	"created_at": "Sun Dec 20 17:18:10 +0000 2020",
+	"tweets": 24176,
+	"created_at": "Mon Dec 21 17:18:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,373 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 21 12:48:22 +0000 2020",
+		"id": 1341002572522352600,
+		"id_str": "1341002572522352641",
+		"full_text": "rt 요새 진비우가 부쩍 좋아짐. 키도 크고 목소리도 좋고 무엇보다 어리다! 알티한 영상은 끝에 패왕별희 음악이 흐를 때 소름끼침(진비우는 첸카이거 감독 아들",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★핀골피니안x페아노리안 위주/(진정령) 명결회상 중심/성인⬆️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 22,
+			"friends_count": 60,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "rt 요새 진비우가 부쩍 좋아짐. 키도 크고 목소리도 좋고 무엇보다 어리다! 알티한 영상은 끝에 패왕별희 음악이 흐를 때 소름끼침(진비우는 첸카이거 감독 아들"
+	},
+	{
+		"created_at": "Mon Dec 21 12:45:20 +0000 2020",
+		"id": 1340990691283898400,
+		"id_str": "1341001808030810113",
+		"full_text": "울 비우 목소리때문에 관심생기신 분들이 계시다니..,., 그럼 이걸 꼭 보셔야!!! \n\n진비우 입덕 영상 넘버원No.1🥳\n\n바자BAZAAR 특별영상 \n《台词课 Monologue in The Palace》\n말대황제, 대명궁사, 행복한왕자, 햄릿 속 대사 독백\n\n진비우 목소리 한번만 느껴봐여😭 \n(번역의 어색함은 너그러이~) https://t.co/whMzmn83k2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1340990552968364000,
+					"id_str": "1340990552968364032",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1340990552968364032/pu/img/W38DP0zqZpAXgA2E.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1340990552968364032/pu/img/W38DP0zqZpAXgA2E.jpg",
+					"url": "https://t.co/whMzmn83k2",
+					"display_url": "pic.twitter.com/whMzmn83k2",
+					"expanded_url": "https://twitter.com/broken_gods/status/1340990691283898368/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1340990552968364000,
+					"id_str": "1340990552968364032",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1340990552968364032/pu/img/W38DP0zqZpAXgA2E.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1340990552968364032/pu/img/W38DP0zqZpAXgA2E.jpg",
+					"url": "https://t.co/whMzmn83k2",
+					"display_url": "pic.twitter.com/whMzmn83k2",
+					"expanded_url": "https://twitter.com/broken_gods/status/1340990691283898368/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 140000,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340990552968364032/pu/vid/640x360/QFJqhzjEPpio1O1T.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340990552968364032/pu/vid/480x270/jxWXcap-fpQssMz6.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1340990552968364032/pu/vid/1280x720/JpixdXwl1sFRi03J.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1340990552968364032/pu/pl/BuW3fc035rf_tbVW.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1340478841244930000,
+		"in_reply_to_status_id_str": "1340478841244930048",
+		"in_reply_to_user_id": 895984625532547100,
+		"in_reply_to_user_id_str": "895984625532547072",
+		"in_reply_to_screen_name": "broken_gods",
+		"user": {
+			"id": 895984625532547100,
+			"id_str": "895984625532547072",
+			"name": "고장남신古装男神",
+			"screen_name": "broken_gods",
+			"location": "",
+			"description": "키워드)무협 진비우陈飞宇 중드 중연 중음                    \n        덕질만 합니다 혐오전시, 안티발 루머유포 차단",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 624,
+			"friends_count": 52,
+			"listed_count": 15,
+			"created_at": "Fri Aug 11 12:25:56 +0000 2017",
+			"favourites_count": 28952,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 19067,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1340692579143192577/2wEIzheR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1340692579143192577/2wEIzheR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/895984625532547072/1589701000",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 102,
+		"favorite_count": 89,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "울 비우 목소리때문에 관심생기신 분들이 계시다니..,., 그럼 이걸 꼭 보셔야!!! \n\n진비우 입덕 영상 넘버원No.1🥳\n\n바자BAZAAR 특별영상 \n《台词课 Monologue in The Palace》\n말대황제, 대명궁사, 행복한왕자, 햄릿 속 대사 독백\n\n진비우 목소리 한번만 느껴봐여😭 \n(번역의 어색함은 너그러이~) https://t.co/whMzmn83k2"
+	},
+	{
+		"created_at": "Mon Dec 21 05:16:28 +0000 2020",
+		"id": 1340888846934757400,
+		"id_str": "1340888846934757378",
+		"full_text": "@ga2nobi 분명 수작인데 재탕할 엄두가 안 날 정도로 정신력을 소모하는 드라마에요ㅋㅋㅋ 진짜 아들을...남보다 더 괴롭힙니다^^",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ga2nobi",
+					"name": "영원한 절정지옥",
+					"id": 1207478451776577500,
+					"id_str": "1207478451776577538",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1340882646604206000,
+		"in_reply_to_status_id_str": "1340882646604206080",
+		"in_reply_to_user_id": 1207478451776577500,
+		"in_reply_to_user_id_str": "1207478451776577538",
+		"in_reply_to_screen_name": "ga2nobi",
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨덕)★핀골페아★핀골피니안x페아노리안 위주/(진정령) 명결회상 중심/성인⬆️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 22,
+			"friends_count": 60,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1261990791322329090/vHnb14Q2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "@ga2nobi 분명 수작인데 재탕할 엄두가 안 날 정도로 정신력을 소모하는 드라마에요ㅋㅋㅋ 진짜 아들을...남보다 더 괴롭힙니다^^"
+	},
+	{
 		"created_at": "Sun Dec 20 14:51:46 +0000 2020",
 		"id": 1340671237119307800,
 		"id_str": "1340671237119307778",
