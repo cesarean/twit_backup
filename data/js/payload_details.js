1edd341043dbacacd84c2a0f40db00b0dd1adde8
cesarean <cesareacs@gmail.com>
2021-04-02 02:18:13 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24723,
-	"created_at": "Wed Mar 31 17:18:12 +0000 2021",
+	"tweets": 24726,
+	"created_at": "Thu Apr 01 17:18:13 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,246 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Thu Apr 01 16:50:04 +0000 2021",
+		"id": 1377660661501587500,
+		"id_str": "1377664570437734400",
+		"full_text": "어린이 답선군 https://t.co/aioaRT9dse",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			7
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1377660480227926000,
+					"id_str": "1377660480227926017",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/media/Ex5u6FkUYAEBIYl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ex5u6FkUYAEBIYl.jpg",
+					"url": "https://t.co/aioaRT9dse",
+					"display_url": "pic.twitter.com/aioaRT9dse",
+					"expanded_url": "https://twitter.com/irkoo09/status/1377660661501587459/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 313,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 1087,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 1087,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1377660480227926000,
+					"id_str": "1377660480227926017",
+					"indices": [
+						8,
+						31
+					],
+					"media_url": "http://pbs.twimg.com/media/Ex5u6FkUYAEBIYl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ex5u6FkUYAEBIYl.jpg",
+					"url": "https://t.co/aioaRT9dse",
+					"display_url": "pic.twitter.com/aioaRT9dse",
+					"expanded_url": "https://twitter.com/irkoo09/status/1377660661501587459/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 313,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 1087,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 1087,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1680416480,
+			"id_str": "1680416480",
+			"name": "Rk",
+			"screen_name": "irkoo09",
+			"location": "아이콘-하와와님 /헤더-강도님",
+			"description": "Please do not repost\n차단당해있다면 체인블락에 휩쓸린것\nrt정말많이함/구독\n잡덕계정((장르계아님!!)) 팔로우비추\n그때그때 좋아하는걸로 도배하는 편",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11876,
+			"friends_count": 250,
+			"listed_count": 67,
+			"created_at": "Sun Aug 18 10:37:12 +0000 2013",
+			"favourites_count": 24663,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12206,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1338509880404000768/WqmKa9BX_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1338509880404000768/WqmKa9BX_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1680416480/1614320390",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 120,
+		"favorite_count": 215,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "어린이 답선군 https://t.co/aioaRT9dse"
+	},
+	{
+		"created_at": "Thu Apr 01 16:17:22 +0000 2021",
+		"id": 1377656344749088800,
+		"id_str": "1377656344749088768",
+		"full_text": "얼하 외전 보니 답선군놈 남병산에서도 초만녕한테 미약 먹이고 있던데ㅋㅋㅋ만고정독까진 아니더라도 마음이 통한 시점에 약까지 먹으면 초만녕 진짜 있는 소리 없는 소리 다 내며 조를 것 같다. 답선군 너무 좋아서 머리가 핑 돌 정도로 불타오를듯.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨킨)★핀골페아★핀골피니안x페아노리안 위주/(얼하·호의행)묵연만녕·페이윈⬆️성인⬆️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 22,
+			"friends_count": 56,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 5637,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24485,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366650939571019777/QKdVJxOM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366650939571019777/QKdVJxOM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "얼하 외전 보니 답선군놈 남병산에서도 초만녕한테 미약 먹이고 있던데ㅋㅋㅋ만고정독까진 아니더라도 마음이 통한 시점에 약까지 먹으면 초만녕 진짜 있는 소리 없는 소리 다 내며 조를 것 같다. 답선군 너무 좋아서 머리가 핑 돌 정도로 불타오를듯."
+	}
+]
