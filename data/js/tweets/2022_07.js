b5873e8b047fec59456a07019c72fa8afe9cfdd5
cesarean <cesareacs@gmail.com>
2022-07-03 02:18:11 +0900

    Added 1 tweets

---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,201 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Sat Jul 02 14:13:39 +0000 2022",
+		"id": 1541573920905494500,
+		"id_str": "1543236464732803072",
+		"full_text": "내가 예상했던                    실제\n탑건2 내용                 탑건2 내용 https://t.co/6lMsqqD6fL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1541573914450886700,
+					"id_str": "1541573914450886657",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTFTr3agAETMl8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTFTr3agAETMl8.jpg",
+					"url": "https://t.co/6lMsqqD6fL",
+					"display_url": "pic.twitter.com/6lMsqqD6fL",
+					"expanded_url": "https://twitter.com/Yugyeo_man/status/1541573920905494528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1541573914450886700,
+					"id_str": "1541573914450886657",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTFTr3agAETMl8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTFTr3agAETMl8.jpg",
+					"url": "https://t.co/6lMsqqD6fL",
+					"display_url": "pic.twitter.com/6lMsqqD6fL",
+					"expanded_url": "https://twitter.com/Yugyeo_man/status/1541573920905494528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 657,
+							"h": 606,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1541573917739200500,
+					"id_str": "1541573917739200512",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTFT4HaMAA6tMy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTFT4HaMAA6tMy.jpg",
+					"url": "https://t.co/6lMsqqD6fL",
+					"display_url": "pic.twitter.com/6lMsqqD6fL",
+					"expanded_url": "https://twitter.com/Yugyeo_man/status/1541573920905494528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 592,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 704,
+							"h": 613,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 704,
+							"h": 613,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1036284227585597400,
+			"id_str": "1036284227585597441",
+			"name": "유겨",
+			"screen_name": "Yugyeo_man",
+			"location": "",
+			"description": "IMF다니고시키는거다해요\nㄴIMF퇴사해",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64,
+			"friends_count": 26,
+			"listed_count": 1,
+			"created_at": "Sun Sep 02 16:06:27 +0000 2018",
+			"favourites_count": 2116,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4217,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1542899693318901762/i8BVEREF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542899693318901762/i8BVEREF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1036284227585597441/1656535119",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 641,
+		"favorite_count": 401,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "내가 예상했던                    실제\n탑건2 내용                 탑건2 내용 https://t.co/6lMsqqD6fL"
+	}
+]
