148d33caaebbd5a70cb126a9b263a18d4b9cb6c1
cesarean <cesareacs@gmail.com>
2022-09-28 02:18:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27127,
-	"created_at": "Sat Sep 17 17:18:10 +0000 2022",
+	"tweets": 27128,
+	"created_at": "Tue Sep 27 17:18:11 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 27 13:38:52 +0000 2022",
+		"id": 1574728513931792400,
+		"id_str": "1574755454580060162",
+		"full_text": "I'm uploading a cartoon I drew before to celebrate XQC’s birthday. I'm going to delete it tomorrow~ (It's in Korean~)\n\nhttps://t.co/SQoZIjGgSH https://t.co/wo4obYqLan",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SQoZIjGgSH",
+					"expanded_url": "https://www.pixiv.net/artworks/101527803",
+					"display_url": "pixiv.net/artworks/10152…",
+					"indices": [
+						119,
+						142
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1574728506847637500,
+					"id_str": "1574728506847637504",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/FdqPO9RVsAAOabz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdqPO9RVsAAOabz.jpg",
+					"url": "https://t.co/wo4obYqLan",
+					"display_url": "pic.twitter.com/wo4obYqLan",
+					"expanded_url": "https://twitter.com/_133113_1/status/1574728513931792384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 407,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 588,
+							"h": 983,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 588,
+							"h": 983,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1574728506847637500,
+					"id_str": "1574728506847637504",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/FdqPO9RVsAAOabz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdqPO9RVsAAOabz.jpg",
+					"url": "https://t.co/wo4obYqLan",
+					"display_url": "pic.twitter.com/wo4obYqLan",
+					"expanded_url": "https://twitter.com/_133113_1/status/1574728513931792384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 407,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 588,
+							"h": 983,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 588,
+							"h": 983,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1511275678549053400,
+			"id_str": "1511275678549053442",
+			"name": "13🍊 (ᐡ o̴̶̷̤ ﻌ o̴̶̷̤ ᐡ)ଳ ʕ - ᴥ - ʔ",
+			"screen_name": "_133113_1",
+			"location": "Please don't repost and print",
+			"description": "main account:: @_133113_",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2677,
+			"friends_count": 1,
+			"listed_count": 7,
+			"created_at": "Tue Apr 05 09:33:03 +0000 2022",
+			"favourites_count": 171,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 213,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1511495472720269313/YYRuIvpQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1511495472720269313/YYRuIvpQ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 79,
+		"favorite_count": 250,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm uploading a cartoon I drew before to celebrate XQC’s birthday. I'm going to delete it tomorrow~ (It's in Korean~)\n\nhttps://t.co/SQoZIjGgSH https://t.co/wo4obYqLan"
+	},
+	{
 		"created_at": "Sat Sep 17 07:11:49 +0000 2022",
 		"id": 1570723828812378000,
 		"id_str": "1571034170872434692",
