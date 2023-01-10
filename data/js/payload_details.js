cebe17d931b2b3ffe2571e841bd3bd3001231e10
cesarean <cesareacs@gmail.com>
2023-01-11 02:18:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27330,
-	"created_at": "Mon Jan 09 17:18:11 +0000 2023",
+	"tweets": 27331,
+	"created_at": "Tue Jan 10 17:18:10 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Tue Jan 10 12:32:19 +0000 2023",
+		"id": 1612789432129839000,
+		"id_str": "1612789432129839106",
+		"full_text": "불꽃남자의 유혹이 정말 어마어마하다...근 이십년만에 다시 본 정대만에 또 빠져들게 되다니...불꽃남자 이 남자의 도화살은 대체 어디까지... https://t.co/iLoCuKZs95",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1612789428048760800,
+					"id_str": "1612789428048760838",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/FmHHcJxaEAYbO_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmHHcJxaEAYbO_N.jpg",
+					"url": "https://t.co/iLoCuKZs95",
+					"display_url": "pic.twitter.com/iLoCuKZs95",
+					"expanded_url": "https://twitter.com/celebaduial/status/1612789432129839106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 736,
+							"h": 1150,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 736,
+							"h": 1150,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1612789428048760800,
+					"id_str": "1612789428048760838",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/FmHHcJxaEAYbO_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmHHcJxaEAYbO_N.jpg",
+					"url": "https://t.co/iLoCuKZs95",
+					"display_url": "pic.twitter.com/iLoCuKZs95",
+					"expanded_url": "https://twitter.com/celebaduial/status/1612789432129839106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 736,
+							"h": 1150,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 736,
+							"h": 1150,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2327283314,
+			"id_str": "2327283314",
+			"name": "앵무",
+			"screen_name": "celebaduial",
+			"location": "",
+			"description": "(톨킨)★핀골페아★핀골피니안x페아노리안/(얼하·호의행)묵연만녕·페이윈/(병안본)위칭 https://t.co/jKrr84pgrb ⬆️성인⬆️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/jKrr84pgrb",
+							"expanded_url": "https://psittacus.postype.com",
+							"display_url": "psittacus.postype.com",
+							"indices": [
+								47,
+								70
+							]
+						}
+					]
+				}
+			},
+			"protected": true,
+			"followers_count": 27,
+			"friends_count": 65,
+			"listed_count": 0,
+			"created_at": "Tue Feb 04 15:20:49 +0000 2014",
+			"favourites_count": 7499,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27355,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1500447576025305088/kC2bV5ay_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1500447576025305088/kC2bV5ay_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2327283314/1402311429",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "불꽃남자의 유혹이 정말 어마어마하다...근 이십년만에 다시 본 정대만에 또 빠져들게 되다니...불꽃남자 이 남자의 도화살은 대체 어디까지... https://t.co/iLoCuKZs95"
+	},
+	{
 		"created_at": "Mon Jan 09 10:47:12 +0000 2023",
 		"id": 1612400589870366700,
 		"id_str": "1612400589870366723",
