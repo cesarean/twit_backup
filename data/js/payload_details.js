eb877e2d49e6b898b90b1988dbe7f720d62a43c2
cesarean <cesareacs@gmail.com>
2023-05-18 02:18:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28399,
-	"created_at": "Tue May 16 17:18:11 +0000 2023",
+	"tweets": 28400,
+	"created_at": "Wed May 17 17:18:11 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Wed May 17 15:35:15 +0000 2023",
+		"id": 1658457630703235000,
+		"id_str": "1658858729700458496",
+		"full_text": "준섭성지대만\n슈팅가드즈 해달라고 울기(짤참고) https://t.co/5sgfqoTJrB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1658457627565883400,
+					"id_str": "1658457627565883401",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/media/FwQGbOYaAAkQIam.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FwQGbOYaAAkQIam.jpg",
+					"url": "https://t.co/5sgfqoTJrB",
+					"display_url": "pic.twitter.com/5sgfqoTJrB",
+					"expanded_url": "https://twitter.com/a6cd_/status/1658457630703235074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1846,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 613,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1658457627565883400,
+					"id_str": "1658457627565883401",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/media/FwQGbOYaAAkQIam.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FwQGbOYaAAkQIam.jpg",
+					"url": "https://t.co/5sgfqoTJrB",
+					"display_url": "pic.twitter.com/5sgfqoTJrB",
+					"expanded_url": "https://twitter.com/a6cd_/status/1658457630703235074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1846,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 613,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1635547033284255700,
+			"id_str": "1635547033284255745",
+			"name": "abcd",
+			"screen_name": "a6cd_",
+			"location": "",
+			"description": "기운날때.. 그림을 그립니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 137,
+			"friends_count": 13,
+			"listed_count": 1,
+			"created_at": "Tue Mar 14 07:43:00 +0000 2023",
+			"favourites_count": 10,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 823,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1642859217546772480/2pe_CKUG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1642859217546772480/2pe_CKUG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1635547033284255745/1679804461",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 898,
+		"favorite_count": 1215,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "준섭성지대만\n슈팅가드즈 해달라고 울기(짤참고) https://t.co/5sgfqoTJrB"
+	},
+	{
 		"created_at": "Mon May 15 23:59:54 +0000 2023",
 		"id": 1658052786171560000,
 		"id_str": "1658260950229876739",
